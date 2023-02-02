4eaa66dc6575e3af07965ea176b243da1cb8f12e
ruslankorotkov <richard2008@rus>
2023-02-02 12:30:07 +0300

    Введение в Maven и сборку проектов

---
+++ src/test/java/sky/pro/recipesapp/RecipesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.recipesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
