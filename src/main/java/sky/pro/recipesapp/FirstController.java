b384e30d45848091f382816ab6706f5ba38a20f6
ruslankorotkov <richard2008@rus>
2023-02-02 12:42:08 +0300

    Введение в Maven и сборку проектов RecipesApp - приложение для сайта рецептов. 02/02/2023

--- src/main/java/sky/pro/recipesapp/FirstController.java
+++ src/main/java/sky/pro/recipesapp/FirstController.java
@@ -1,6 +1,7 @@
 package sky.pro.recipesapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,14 +9,21 @@ public class FirstController {
 
 
     @GetMapping
-    public String applicationLaunched(){
+    public String applicationLaunched() {
         return " Приложение запущено ";
     }
+
     @GetMapping("/info")
-    public String info(){
+    public String info() {
         return " info: имя ученика: Руслан ,\n" +
                 "название вашего проекта: RecipesApp ,\n" +
                 "дату создания проекта: 02/02/2023 ,\n" +
                 "описание проекта: приложение для сайта рецептов";
     }
+
+    @GetMapping("/info")
+    public String info(@RequestParam String info) {
+        return " info:" + info;
+    }
+
 }
