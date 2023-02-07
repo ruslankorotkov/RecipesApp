8f73caacd58808eed60947077584e786fcefcfe9
ruslankorotkov <richard2008@rus>
2023-02-07 10:26:41 +0300

    Структура веб-приложения. RecipesApp - приложение для сайта рецептов. Домашняя работа от 06/02/2023

--- src/main/java/sky/pro/recipesapp/controllers/RecipesController.java
+++ src/main/java/sky/pro/recipesapp/controllers/RecipesController.java
@@ -9,15 +9,4 @@ import java.util.Map;
 @RestController
 @RequestMapping
 public class RecipesController {
-    @GetMapping("/api")
-    @ResponseBody
-    public String getMapVariable(@PathVariable Map recipesMap) {
-        Integer id = (Integer) recipesMap.get("id");
-        String title = (String) recipesMap.get("title");
-        if(id != null && title  != null) {
-            return "ID: " + id + ", title: " + title;
-        } else {
-            return "Missing Parameters";
-        }
-    }
 }
