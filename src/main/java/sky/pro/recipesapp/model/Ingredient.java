19d254c47c72ec37576afb79ded8b6720430c65c
ruslankorotkov <richard2008@rus>
2023-02-08 12:32:22 +0300

    Структура веб-приложения. Создание API. Postman RecipesApp - приложение для сайта рецептов. Домашняя работа от 08/02/2023

--- src/main/java/sky/pro/recipesapp/model/Ingredient.java
+++ src/main/java/sky/pro/recipesapp/model/Ingredient.java
@@ -1,7 +1,9 @@
 package sky.pro.recipesapp.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
 @AllArgsConstructor
 public class Ingredient {
     private String name;
--- src/main/java/sky/pro/recipesapp/model/Recipe.java
+++ src/main/java/sky/pro/recipesapp/model/Recipe.java
@@ -1,9 +1,10 @@
 package sky.pro.recipesapp.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.util.List;
-
+@Data
 @AllArgsConstructor
 public class Recipe {
     private String title;
