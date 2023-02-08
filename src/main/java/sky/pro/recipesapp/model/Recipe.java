c561d532e3eca4a4686d3a4825e212626dbeefc1
ruslankorotkov <richard2008@rus>
2023-02-08 16:21:03 +0300

    Структура веб-приложения. Создание API. Postman Подключение библиотек RecipesApp - приложение для сайта рецептов. Домашняя работа от 08/02/2023

--- src/main/java/sky/pro/recipesapp/model/Recipe.java
+++ src/main/java/sky/pro/recipesapp/model/Recipe.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Map;
+
 @Data
 @AllArgsConstructor
 public class Recipe {
     private String title;
     private int cookingTime;
-    private List<Ingredient> ingredients;
-    private List<String> cookingInstructionsSteps;
+    private Map<Long, Ingredient> ingredientMap;
+    private Map<Long, String> cookingInstructionsSteps;
 }
