3298a4ec6368350506fb98c59005069f73ace268
ruslankorotkov <richard2008@rus>
2023-02-11 19:21:18 +0300

    Структура веб-приложения. Создание API. Postman Подключение библиотек RecipesApp - приложение для сайта рецептов. Домашняя работа от 08/02/2023 (доделка от 11.02.2023) Я правильно тебя понял и сделал?

--- src/main/java/sky/pro/recipesapp/controllers/IngredientController.java
+++ src/main/java/sky/pro/recipesapp/controllers/IngredientController.java
@@ -23,36 +23,56 @@ public class IngredientController {
         this.ingredientService = ingredientService;
     }
 
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Информация была получена", content = {@Content(mediaType = "application/json")})})
-    @Operation(method = "Добавление ингредиента.", summary = "Можете ввести информацию об новом ингредиент", description = "Можно ввести информацию")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",
+            description = "Информация была получена", content = {@Content(mediaType = "application/json")})})
+    @Operation(method = "Добавление ингредиента.", summary = "Можете ввести информацию об новом ингредиент",
+            description = "Можно ввести информацию")
     @PostMapping("/")
     public ResponseEntity<Ingredient> createIngredient(@RequestBody Ingredient ingredient) {
         return ResponseEntity.ok(ingredientService.createIngredient(ingredient));
     }
 
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Информация была получена", content = {@Content(mediaType = "application/json")})})
-    @Operation(method = "Получение информации об ингредиенте по id.", summary = "Можете получить информацию об ингредиенте по id", description = "Можно получить информацию")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",
+            description = "Информация была получена", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "Информация не была получена",
+                    content = {@Content(mediaType = "application/json")})})
+    @Operation(method = "Получение информации об ингредиенте по id.",
+            summary = "Можете получить информацию об ингредиенте по id", description = "Можно получить информацию")
     @GetMapping("/{id}")
     public ResponseEntity<Ingredient> getIngredient(@PathVariable Long id) {
         return ResponseEntity.of(ingredientService.getId(id));
     }
 
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Информация была получена", content = {@Content(mediaType = "application/json")})})
-    @Operation(method = "Редактирование ингредиента по id.", summary = "Редактирование ингредиента по id.", description = "Можно изменить информацию")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",
+            description = "Информация была получена", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "Информация не была получена",
+                    content = {@Content(mediaType = "application/json")})})
+    @Operation(method = "Редактирование ингредиента по id.",
+            summary = "Редактирование ингредиента по id.", description = "Можно изменить информацию")
     @PutMapping("/{id}")
     public ResponseEntity<Ingredient> updateIngredient(@PathVariable Long id, @RequestBody Ingredient ingredient) {
         return ResponseEntity.of(ingredientService.updateIngredient(id, ingredient));
     }
 
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Информация была получена", content = {@Content(mediaType = "application/json")})})
-    @Operation(method = "Удаление ингредиента.", summary = "Удаление ингредиента.", description = "Можно удалить информацию")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Информация была получена",
+            content = {@Content(mediaType = "application/json")}), @ApiResponse(responseCode = "404",
+            description = "Информация не была получена",
+            content = {@Content(mediaType = "application/json")})})
+    @Operation(method = "Удаление ингредиента.", summary = "Удаление ингредиента.",
+            description = "Можно удалить информацию")
     @DeleteMapping("/{id}")
     public ResponseEntity<Ingredient> deleteIngredient(@PathVariable Long id) {
         return ResponseEntity.of(ingredientService.deleteIngredient(id));
     }
 
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Информация была получена", content = {@Content(mediaType = "application/json")})})
-    @Operation(method = "Получение полного списка ингредиентов.", summary = "Получение полного списка ингредиентов.", description = "Можно получить информацию")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Информация была получена",
+            content = {@Content(mediaType = "application/json")}), @ApiResponse(responseCode = "404",
+            description = "Информация не была получена",
+            content = {@Content(mediaType = "application/json")})})
+    @Operation(method = "Получение полного списка ингредиентов.",
+            summary = "Получение полного списка ингредиентов.", description = "Можно получить информацию")
     @GetMapping("/")
     public ResponseEntity<Map<Long, Ingredient>> getAllIngredients() {
         return ResponseEntity.ok(ingredientService.allIngredients());
