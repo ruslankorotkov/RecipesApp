2e2b7f6bc10d1c63fd15f084f3d8c5922d9652b0
ruslankorotkov <richard2008@rus>
2023-02-13 23:49:25 +0300

    Так норм.? Структура веб-приложения. Создание API. Postman Подключение библиотек RecipesApp - приложение для сайта рецептов. Домашняя работа от 13/02/2023 (Утилиты библиотеки apache commons на валидацию полей данных приходящих на наличие null или пустых строк)

--- src/main/java/sky/pro/recipesapp/exception/ValidationException.java
+++ src/main/java/sky/pro/recipesapp/exception/ValidationException.java
@@ -2,6 +2,6 @@ package sky.pro.recipesapp.exception;
 
 public class ValidationException extends RuntimeException{
     public ValidationException(String e){
-        super("Ошибка наличия данных" + e);
+        super("Ошибка наличия данных " + e);
     }
 }
